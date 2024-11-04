73f4ef86cf22a4717f538d381d5726c3eed1e70f
Saurav Pandey <sp4398@gmail.com>
2024-11-04 21:58:02 +0530

    using useState

--- src/App.js
+++ src/App.js
@@ -2,33 +2,6 @@ import "./App.css";
 import Heading from "./components/Heading";
 import Body from "./components/Body";
 
-const Heading = () => {
-  return (
-    <div className="header">
-      <div className="logo">
-        <img
-          className="logo-img"
-          alt="img"
-          src="https://media.istockphoto.com/id/1435983029/vector/food-delivery-logo-images.jpg?s=612x612&w=0&k=20&c=HXPxcjOxUiW4pMW1u9E0k2dJYQOU37a_0qZAy3so8fY="
-        />
-      </div>
-      <div className="name"><h1>food-Hub</h1></div>
-      
-      <div className="nav-items">
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact Us</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
-
-
-
-
 function App() {
   return (
     <div className="App">
--- src/components/ResCard.js
+++ src/components/ResCard.js
@@ -1,32 +1,32 @@
+import { CDN_URL } from "../utils/constants";
+
 const ResCard = (props) => {
-    const { resData } = props;
-    const { name, avgRating, cuisines, cloudinaryImageId } = resData.info;
-    return (
-      <div className="res-card">
-        <div className="card-img">
-          <img
-            className="cardImg"
-            src={
-              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/" +cloudinaryImageId
-            }
-            alt="cardImg"
-          />
-        </div>
-        <div className="res-detail">
-          <div className="res-title">{name}</div>
-          <span>
-            ⭐{avgRating}
-            <span className="distance">
-              &nbsp;{resData.info.sla.deliveryTime}mins
-            </span>
+  const { resData } = props;
+  const { name, avgRating, cuisines, cloudinaryImageId } = resData.info;
+  return (
+    <div className="res-card">
+      <div className="card-img">
+        <img
+          className="cardImg"
+          src={CDN_URL + cloudinaryImageId}
+          alt="cardImg"
+        />
+      </div>
+      <div className="res-detail">
+        <div className="res-title">{name}</div>
+        <span>
+          ⭐{avgRating}
+          <span className="distance">
+            &nbsp;{resData.info.sla.deliveryTime}mins
           </span>
-          <div className="desc">
-            {cuisines.join(", ")}
-            <br />
-          </div>
+        </span>
+        <div className="desc">
+          {cuisines.join(", ")}
+          <br />
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default ResCard;
+export default ResCard;
