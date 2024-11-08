e800b27f63d2b43e707cbfd0ac29d5f1ba5abba0
Saurav Pandey <sp4398@gmail.com>
2024-11-08 23:47:50 +0530

    changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,10 +15,11 @@ const Body = () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
-    const json=await data.json()
+    const json = await data.json();
     console.log(json);
-    setListOfRes(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-    
+    setListOfRes(
+      json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants
+    );
   };
 
   return (
